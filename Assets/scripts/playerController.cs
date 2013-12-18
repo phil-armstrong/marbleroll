f4281c9aa831d75ba4def7e489c27b6f56bec0b6
Phil Armstrong <phil.armstrong1@gmail.com>
2013-12-18 22:44:18 +0000

    Episode 5 & 6

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class playerController : MonoBehaviour {
 	public float speed;
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start() {
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
 
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis("Horizontal");
@@ -11,5 +20,20 @@ public class playerController : MonoBehaviour {
 		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
 
 		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString();
+		if (count >= 4) {
+			winText.text = "YOU WIN!";
+		}
 	}
 }
