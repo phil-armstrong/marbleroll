8eb6b712b77a459de2fc9cbf3cc4ee61b5ad40b7
Phil Armstrong <phil.armstrong1@gmail.com>
2013-12-18 22:48:56 +0000

    mend

--- Assets/scripts/cameraController.cs
+++ Assets/scripts/cameraController.cs
@@ -5,12 +5,11 @@ public class cameraController : MonoBehaviour {
 
 	public GameObject player;
 	private Vector3 offset;
-	// Use this for initialization
+
 	void Start () {
 		offset = transform.position;
 	}
-	
-	// Update is called once per frame
+
 	void LateUpdate () {
 		transform.position = player.transform.position + offset;
 	}
